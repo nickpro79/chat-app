3045368311b5e9d74ab25bfe77a2855f1ab9ea6c
nickpro79 <108288025+nickpro79@users.noreply.github.com>
2025-10-28 23:23:44 +0530

    added db.js for database connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,16 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
+const PORT = process.env.PORT;
+
 
 app.use("/api/auth",authRoutes);
 
 app.listen(5000,()=>{
-    console.log("Server is running on port 5000");
+    console.log("Server is running on PORT:"+PORT);
+    connectDB();
 })
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -7,7 +7,4 @@ router.get("/signup",signup);
 router.get("/login",login);
 router.get("/logout",logout);
 
-
-
-
 export default router;
