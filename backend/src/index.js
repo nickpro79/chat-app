dec2c631b6f209f4178292ce023f9a93fd35be15
nickpro79 <108288025+nickpro79@users.noreply.github.com>
2025-10-30 23:37:11 +0530

    added cors and axios

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 
@@ -10,6 +11,10 @@ dotenv.config();
 const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}))
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
