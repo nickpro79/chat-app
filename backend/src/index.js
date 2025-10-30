daaeed7d6288fca790844a6945f966b211f26f25
Nikhil <nikhilpraveen7777@gmail.com>
2025-10-30 12:57:17 +0530

    created user model, created signup logic and tested /signup endpoint

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,11 +6,11 @@ import dotenv from "dotenv";
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT;
+app.use(express.json());
 
+app.use("/api/auth", authRoutes);
 
-app.use("/api/auth",authRoutes);
-
-app.listen(5000,()=>{
-    console.log("Server is running on PORT:"+PORT);
-    connectDB();
-})
+app.listen(PORT, () => {
+  console.log("Server is running on PORT:" + PORT);
+  connectDB();
+});
