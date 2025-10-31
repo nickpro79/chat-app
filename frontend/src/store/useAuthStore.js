b662bbf180015d6876c3ccc057a96847c0f289c0
Nikhil Praveen <nikhilpraveen7777@gmail.com>
2025-10-31 18:46:21 +0530

    fixed bugs

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -56,4 +56,17 @@ export const useAuthStore = create((set) => ({
       toast.error(error.response.data.message);
     }
   },
+  updateProfile: async (data) => {
+    set({isUpdatingProfile: True});
+    try {
+      const res = await axiosInstance.put("/auth/update-profile",data)
+      set({ authUser: res.data})
+      toast.success("Profile updated successfully")
+    } catch (error) {
+      console.log("error in updateprofile",error);
+      toast.error(error.response.data.message);
+    }finally{
+      set({isUpdatingProfile: false})
+    }
+  }
 }));
