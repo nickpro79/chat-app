5b65bc4b60a5125055349505d32597aab9ffb0f3
Nikhil Praveen <nikhilpraveen7777@gmail.com>
2025-10-31 16:19:20 +0530

    fixed bugs in SignUpPage

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore";
+import {Eye,EyeOff,Loader2,Lock,Mail,MessageSquare,User} from 'lucide-react'
+import { Link } from 'react-router-dom'
 
 const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false)
@@ -9,7 +11,8 @@ const SignUpPage = () => {
     password:""
   })
 
-  const [ signup, isSigningUp ] = useAuthStore();
+  const signup = useAuthStore((state) => state.signup);
+ const isSigningUp = useAuthStore((state) => state.isSigningUp);
   const validateForm = () => {}
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -122,12 +125,7 @@ const SignUpPage = () => {
         </div>
       </div>
 
-      {/* right side */}
 
-      <AuthImagePattern
-        title="Join our community"
-        subtitle="Connect with friends, share moments, and stay in touch with your loved ones."
-      />
     </div>
   );
 };
