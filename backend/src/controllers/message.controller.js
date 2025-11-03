843f1c92a70ff447d5809448842c22c222054b05
Nikhil <nikhilpraveen7777@gmail.com>
2025-11-03 17:32:57 +0530

    completed the project, needs testing

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,6 +1,7 @@
 import cloudinary from "../lib/cloudinary.js";
 import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
+import { getReceiverSocketId, io } from "../lib/socket.js";
 
 export const getUsersForSidebar = async (req, res) => {
   try {
@@ -52,7 +53,12 @@ export const sendMessage = async (req, res) => {
     });
 
     await newMessage.save();
-    //todo ----> socket.io
+
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
+
     res.status(201).json({ newMessage });
   } catch (error) {
     console.log("Error in sendMessage:", error);
--- backend/src/index.js
+++ backend/src/index.js
@@ -5,8 +5,8 @@ import { connectDB } from "./lib/db.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import { app, server } from "./lib/socket.js";
 
-const app = express();
 dotenv.config();
 const PORT = process.env.PORT;
 app.use(express.json({ limit: "10mb" })); // or even 20mb if needed
@@ -22,7 +22,7 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log("Server is running on PORT:" + PORT);
   connectDB();
 });
